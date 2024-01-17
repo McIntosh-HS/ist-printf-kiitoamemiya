6f09be42e6a25c789cbf48680d4ce22931dec420
Kiito Amemiya <2026amemiya.key@fcboe.org>
2024-01-17 15:39:48 -0500

    Finished

--- printf.c
+++ printf.c
@@ -3,13 +3,12 @@
 int main()
 {
 	int a = 10;
-	float f = 1.23;
-	char c = 'z';
 
 	// Format specifiers are placeholders for variable data
-	printf("The value of a is: %i\n", a);	// %i is for integers
-	printf("The value of f is: %f\n", f);	// %f is for floating point data
-	printf("The value of c is: %c\n", c);   // %c is for single characters
-
+	printf("a displayed as base 10 integer: %i\n", a);	// %i is for integers
+	printf("a displayed as base 8 integer: %o\n", a)
+	printf("a displayed as base 16 integer: %x\n", a);
+	printf("a displayed as base 16 integer: %X\n", a);
+	printf("a displayed as base 10 unsigned integer: %u\n", a);
 	return 0;
 }
